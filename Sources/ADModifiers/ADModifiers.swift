360b47ce426a5d2ae2ffedc1962e1524fad9a15d
Alperen <alperenduran@outlook.com>
2020-05-13 18:08:30 +0300

    Implement taptic feedback for iOS Devices.

--- Sources/ADModifiers/ADModifiers.swift
+++ Sources/ADModifiers/ADModifiers.swift
@@ -1,3 +1,10 @@
-struct ADModifiers {
-    var text = "Hello, World!"
+import SwiftUI
+import Foundation
+
+@available(iOS 13.0, *)
+public extension View {
+    func tapticFeedback(_ type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
 }
--- Tests/ADModifiersTests/ADModifiersTests.swift
+++ Tests/ADModifiersTests/ADModifiersTests.swift
@@ -6,7 +6,6 @@ final class ADModifiersTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ADModifiers().text, "Hello, World!")
     }
 
     static var allTests = [
